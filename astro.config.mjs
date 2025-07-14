62aa6b4c6ed3ec5b469c0d1d50f648043d240813
wolfgangschoeffel <wolfgang@unfun.de>
2025-07-14 12:34:57 +0200

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Arimo",
+        cssVariable: "--font-arimo",
+        weights: [400, 700],
+        styles: ["normal"],
+      },
+    ],
+  },
+});
